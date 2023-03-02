510cdb581e9a039b6098e29f68ebd37a1d6fea67
Kanerix <dkkasjoe@hotmail.com>
2023-03-02 13:53:30 +0100

    Better button docs

--- src/components/button.rs
+++ src/components/button.rs
@@ -1,32 +1,39 @@
 use leptos::*;
-use leptos_router::{A, AProps};
+use leptos_router::{AProps, A};
 
 #[component]
 pub fn Button(
 	cx: Scope,
-	#[prop(optional)]
-	class: &'static str,
-	#[prop(optional)]
+	/// Additional classes to add to the button.
+	#[prop(optional, into)]
+	class: Option<String>,
+	/// The text to display on the button.
+	#[prop(optional, into)]
+	text: Option<String>,
 	/// If `to` is empty, the button will be a `<button>` element, else, it will be an `<a>` element.
-	text: &'static str,
-	#[prop(optional)]
-	to: &'static str,
+	#[prop(optional, into)]
+	to: Option<String>,
 ) -> impl IntoView {
-	let styles = format!(
+	let mut styles = format!(
 		"text-white font-bold py-2 px-4 rounded-md
 		bg-gradient-to-t from-blue-700 to-blue-500
 		hover:from-blue-800 hover:to-blue-600 
-		focus:ring focus:ring-blue-500 focus:ring-opacity-50
-		{class}"
+		focus:ring focus:ring-blue-500 focus:ring-opacity-50",
 	);
 
-	if to.is_empty() {
-		view! { cx, 
-			<button class=styles>{text.to_string()}</button>
-		}.into_view(cx)
+	if let Some(class) = class {
+		styles.push_str(&class);
+	}
+
+	if let Some(to) = to {
+		view! { cx,
+			<A href=to class=styles>{text}</A>
+		}
+		.into_view(cx)
 	} else {
 		view! { cx,
-			<A href=to class=styles>{text.to_string()}</A>
-		}.into_view(cx)
+			<button class=styles>{text}</button>
+		}
+		.into_view(cx)
 	}
 }
