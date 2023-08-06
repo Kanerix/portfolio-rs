efd5ca887fcb0cd6efbef65100732353dc094af8
Kasper <57803719+Kanerix@users.noreply.github.com>
2023-08-06 17:36:57 +0200

    Accessibility

--- src/pages/home.rs
+++ src/pages/home.rs
@@ -8,21 +8,21 @@ pub fn Home(cx: Scope) -> impl IntoView {
 				<h1 class="text-5xl font-semibold text-slate-900 dark:text-slate-100">
 					"Hi, im Kasper"
 				</h1>
-				<h4 class="pt-2 text-xl text-slate-600 dark:text-slate-300">
+				<h2 class="pt-2 text-xl text-slate-600 dark:text-slate-300">
 					"Fullstack Developer"
-				</h4>
+				</h2>
 				<p class="pt-4 grow text-slate-500 dark:text-slate-400">
 					"Lorem ipsum dolor sit amet, consectetur adipiscing elit.
 					Donec placerat justo neque, ut accumsan mi tristique in."
 				</p>
 				<div class="grid gap-5 grid-cols-3 mt-8">
-					<a href="https://github.com/Kanerix">
+					<a href="https://github.com/Kanerix" aria-label="Checkout my GitHub">
 						<i class="fa-brands fa-github text-4xl text-slate-600 dark:text-slate-400" />
 					</a>
-					<a href="https://twitter.com/K4nerix">
+					<a href="https://twitter.com/K4nerix" aria-label="Checkout my Twitter">
 						<i class="fa-brands fa-twitter text-4xl text-slate-600 dark:text-slate-400" />
 					</a>
-					<a href="https://www.linkedin.com/in/kasper-jonsson">
+					<a href="https://www.linkedin.com/in/kasper-jonsson" aria-label="Checkout my LinkedIn">
 						<i class="fa-brands fa-linkedin text-4xl text-slate-600 dark:text-slate-400" />
 					</a>
 				</div>
